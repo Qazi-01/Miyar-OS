93eb97fe2ff4a3f696d5e07c5aa9d9458283659b
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-27 11:58:11 +0000

    update to assembly bootloader code

--- src/boot.asm
+++ src/boot.asm
@@ -16,16 +16,16 @@ extern kernel_main
 
 _start:
     mov esp, stack_top
-        push ebx
-            push eax
-                call kernel_main
-                    cli
-                    .hang:
-                        hlt
-                            jmp .hang
+    push ebx
+    push eax
+    call kernel_main
+    cli
+.hang:
+    hlt
+    jmp .hang
 
-                            section .bss
-                            align 16
-                            stack_bottom:
-                                resb 16384
-                                stack_top:
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
