cea182ed38dc48165352baf2d963b02f8f747da5
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-28 03:24:53 +0000

    Clean up

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,1 @@
-#include <stdint.h>
 
-__attribute__((used))
-void kernel_main(uint32_t magic, uint32_t* info) {
-    (void)magic;
-    (void)info;
-
-    /* Force a character out of the serial port without waiting */
-    __asm__ volatile ("outb %0, %1" : : "a"((uint8_t)'X'), "Nd"((uint16_t)0x3F8));
-
-    /* Hang */
-    for (;;) {
-        __asm__ volatile ("hlt");
-    }
