6ddd953a823ae343bc0dfaa438725d1208f22254
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-28 13:15:32 +0000

    patch #3

--- kernel/vga.c
+++ kernel/vga.c
@@ -8,7 +8,8 @@ static uint16_t *vga_buffer = (uint16_t *)VGA_ADDRESS;
 
 static uint8_t color = 0x0F;
 
-static int cursor = 0;
+static int cursor_row = 0;
+static int cursor_col = 0;
 
 static inline uint16_t vga_entry(char c, uint8_t color) {
     return (uint16_t)c | ((uint16_t)color << 8);
@@ -19,15 +20,42 @@ void vga_clear(void) {
         vga_buffer[i] = vga_entry(' ', color);
     }
 
-    cursor = 0;
+    cursor_row = 0;
+    cursor_col = 0;
 }
 
 void vga_putc(char c) {
-    if (cursor >= VGA_WIDTH * VGA_HEIGHT)
+    if (c == '\n') {
+        cursor_col = 0;
+        cursor_row++;
         return;
+    }
 
-    vga_buffer[cursor++] = vga_entry(c, color);
+    if (cursor_row >= VGA_HEIGHT) {
+        cursor_row = VGA_HEIGHT - 1;
+    }
+
+    return;
+    }
+
+    if (cursor_row >= VGA_HEIGHT) {
+        cursor_row = VGA_HEIGHT - 1;
+    }
+
+    vga_buffer[cursor_row * VGA_WIDTH + cursor_col] = vga_entry(c, color);
+
+    cursor_col++;
+
+    if (cursor_col >= VGA_WIDTH) {
+        cursor_col = 0;
+        cursor_row++;
+    }
+
+    if (cursor_row >= VGA_HEIGHT) {
+        cursor_row = VGA_HEIGHT - 1;
+    }
 }
+
 
 void vga_write(const char *str) {
     while (*str) {
