3db0ae9e4bc1ff392988a99b9d3060bea3c54e52
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-29 09:51:13 +0000

    Terminal setup

--- kernel/terminal.c
+++ kernel/terminal.c
@@ -1,5 +1,14 @@
 #include "terminal.h"
+#include "vga.h"
 
 void terminal_init(void)
 {
+    vga_clear();
+
+    vga_set_color(VGA_WHITE, VGA_BLACK);
+}
+
+void terminal_write(const char *str)
+{
+    vga_write(str);
 }
