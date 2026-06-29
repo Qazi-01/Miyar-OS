7290c225639d807f089c002fe921679ce37c4915
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-29 18:26:34 +0000

    patch

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -1,8 +1,23 @@
+#include <stdint.h>
 #include "keyboard.h"
 
 char keyboard_getchar(void)
 {
     return 0;
+}
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t value;
+
+    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
+
+    return value;
+}
+
+void outb(uint16_t port, uint8_t value)
+{
+    __asm__ volatile ("outb %0, %1" :: "a"(value), "Nd"(port));
 }
 
 void keyboard_init(void)
--- kernel/keyboard.h
+++ kernel/keyboard.h
@@ -1,7 +1,12 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include <stdint.h>
+
 char keyboard_getchar(void);
 void keybard_init(void);
+
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t value);
 
 #endif
