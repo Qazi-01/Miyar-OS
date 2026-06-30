c162c0dbf32b66ae3857013c7421a2ccf0fe7c4f
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-30 19:23:22 +0000

    Shell update

--- kernel/shell.c
+++ kernel/shell.c
@@ -19,17 +19,30 @@ static int streq(const char *a, const char *b)
 
 void shell_execute(const char *input)
 {
+    terminal_writeIn("");
+
     if (streq(input, "help"))
     {
-        terminal_writeIn("");
-        terminal_writeIn("Available commands: ");
+        terminal_writeIn("Available commands: ")
         terminal_writeIn("help");
+        terminal_writeIn("about");
+        terminal_writeIn("version");
+    }
+
+    else if (streq(input, "about"))
+    {
+        terminal_writeIn("MiyarOS");
+        terminal_writeIn("A hobby operating system written in C");
+    }
+
+    else if (streq(input, "version"))
+    {
+        terminal_writeIn("MiyarOS v0.1");
     }
 
     else
     {
-        terminal_writeIn("");
-        terminal_writeIn("Unknown command: ");
+        terminal_write("Unknown command: ")
         terminal_writeIn(input);
     }
 }
