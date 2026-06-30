95cbdf9c837d97558c18df609437f2f3652b3297
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-06-30 07:27:11 +0500

    Remove static scancode_table from kernel.c
    Removed the static scancode_table declaration from kernel.c.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,8 +5,6 @@
 #include "terminal.h"
 #include "vga.h"
 
-static const char scancode_table[128] = {0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '0', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ',};
-
 __attribute__((used))
 void kernel_main(uint32_t magic, uint32_t *multiboot_info) {
     (void)magic;
