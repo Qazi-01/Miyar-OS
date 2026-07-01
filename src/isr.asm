cb70cb6cad1c91779b164e250fbeebb0368dfe92
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-02 00:26:02 +0500

    Add additional ISR stubs and macro for ISR handling

--- src/isr.asm
+++ src/isr.asm
@@ -1,15 +1,22 @@
 global isr_stub_0
+global isr_stub_1
+global isr_stub_2
+global isr_stub_3
+
 extern exception_handler
 
 section .text
 
-isr_stub_0:
+%macro ISR_NOERR 1
+isr_stub_%1:
     cli
-
-    pusha                ; Save EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI
-
+    pusha
     call exception_handler
-
-    popa                 ; Restore registers
-
+    popa
     iret
+%endmacro
+
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
