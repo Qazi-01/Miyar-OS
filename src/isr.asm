f259153b6a610890cbd48ca8022812b775bb221a
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-01 16:10:42 +0000

    assembly code patch

--- src/isr.asm
+++ src/isr.asm
@@ -1,18 +1,29 @@
 global isr0
+global isr_common_stub
+
 extern isr0_handler
 
 section .text
 
 isr0:
     cli
+    push byte 0
+    push byte 0
+    jmp isr_common_stub
 
+isr_common_stub:
     pusha
+    
+    move eax, esp
+    push eax
 
-    push esp
     call isr0_handler
+
     add esp, 4
 
     popa
+
+    add esp, 8
 
     iretd
 
