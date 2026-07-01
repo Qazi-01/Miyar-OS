f638d43b263503e9dd2559a1d3803928f18967df
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-01 18:40:52 +0000

    changes made

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "keyboard.h"
+#include "io.h"
 
 static const char scancode_table[128] = {0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0};
 
@@ -24,20 +25,6 @@ char keyboard_getchar(void)
     }
 
     return 0;
-}
-
-uint8_t inb(uint16_t port)
-{
-    uint8_t value;
-
-    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
-
-    return value;
-}
-
-void outb(uint16_t port, uint8_t value)
-{
-    __asm__ volatile ("outb %0, %1" :: "a"(value), "Nd"(port));
 }
 
 void keyboard_init(void)
--- kernel/keyboard.h
+++ kernel/keyboard.h
@@ -6,7 +6,4 @@
 char keyboard_getchar(void);
 void keyboard_init(void);
 
-uint8_t inb(uint16_t port);
-void outb(uint16_t port, uint8_t value);
-
 #endif
