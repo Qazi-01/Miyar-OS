bac0ae622e061e9478f25e5c96153d0f2ff0df34
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-01 05:56:53 +0000

    IDT creation setup

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,4 +1,22 @@
+#include <stdint.h>
 #include "idt.h"
+
+struct idt_entry
+{
+    uint16_t offset_low;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type_attr:
+    uint16_t offset_high;
+} __attribute__((packed));
+
+struct idt_ptr
+{
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+static struct idt_entry idt[256];
 
 void idt_init(void)
 {
