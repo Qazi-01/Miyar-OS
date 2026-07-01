0ec88122ff230613c65972dd470e48d693d0052f
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-01 15:55:05 +0000

    patch

--- kernel/exceptions.h
+++ kernel/exceptions.h
@@ -1,6 +1,20 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
-void isr0_handler(void);
+#include <stdint.h>
+
+struct registers
+{
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+};
+
+void isr0_handler(struct registers *regs);
 
 #endif
