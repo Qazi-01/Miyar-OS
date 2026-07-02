8916c1a882337f3a3649bd5c9d76e7af67ac7f41
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-02 06:24:29 +0500

    Refactor exception handling and add IRQ handler
    Remove panic call on CPU exception and implement infinite loop with halt instruction. Add IRQ handler to manage hardware interrupts.

--- kernel/exceptions.c
+++ kernel/exceptions.c
@@ -1,8 +1,22 @@
 #include "exceptions.h"
-#include "panic.h"
+#include "io.h"
 
 void exception_handler(struct registers *r)
 {
     (void)r;
-    panic("CPU Exception");
+
+    while (1)
+    {
+        __asm__ volatile("hlt");
+    }
+}
+
+void irq_handler(struct registers *r)
+{
+    if (r->int_no >= 40)
+    {
+        outb(0xA0, 0x20);
+    }
+
+    outb(0x20, 0x20);
 }
