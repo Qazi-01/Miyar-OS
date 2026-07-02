178b6c2b03b28c0bc03a63f8e5de6d8bb12eb2ae
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-02 01:28:16 +0000

    bug fix

--- kernel/exceptions.c
+++ kernel/exceptions.c
@@ -10,13 +10,3 @@ void exception_handler(struct registers *r)
         __asm__ volatile("hlt");
     }
 }
-
-void irq_handler(struct registers *r)
-{
-    if (r->int_no >= 40)
-    {
-        outb(0xA0, 0x20);
-    }
-
-    outb(0x20, 0x20);
-}
