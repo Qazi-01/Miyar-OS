4988a374efcd6f8db0ce4787a4d2f3dbad68ac5c
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-02 15:11:29 +0500

    Enhance exception handling with terminal output
    Added exception names and updated exception handler to write to terminal.

--- kernel/exceptions.c
+++ kernel/exceptions.c
@@ -1,12 +1,37 @@
 #include "exceptions.h"
-#include "io.h"
+#include "terminal.h"
+
+static const char *exception_names[] = {
+    "Divide Error",
+    "Debug",
+    "NMI",
+    "Breakpoint",
+    "Overflow",
+    "BOUND",
+    "Invalid Opcode",
+    "Device Not Available",
+    "Double Fault",
+    "Coprocessor Segment",
+    "Invalid TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Reserved",
+    "x87 Floating Point",
+    "Alignment Check",
+    "Machine Check",
+    "SIMD Floating Point",
+    "Virtualization"
+};
 
 void exception_handler(struct registers *r)
 {
-    (void)r;
+    terminal_write("\nEXCEPTION!\n");
+
+    if (r->int_no < 21)
+        terminal_write((char *)exception_names[r->int_no]);
 
     while (1)
-    {
-        __asm__ volatile("hlt");
-    }
+        __asm__ volatile("cli; hlt");
 }
