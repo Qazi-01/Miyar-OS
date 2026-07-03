5584d287d917b078b8dae42dd471195d47df15d6
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-03 05:25:09 +0500

    Refactor irq_handler and add EOI handling

--- kernel/irq.c
+++ kernel/irq.c
@@ -4,6 +4,14 @@
 #include "keyboard.h"
 #include "terminal.h"
 
+void pic_send_eoi(int irq) {
+    outb(0x20, 0x20);   // tell the main chip "done"
+    if (irq >= 8)
+    {
+        outb(0xA0, 0x20); // if it came from the second chip, tell that one too
+    }
+}
+
 static const char scancode_table[128] =
 {
     0, 27,'1','2','3','4','5','6','7','8','9','0','-','=', '\b','\t',
@@ -12,14 +20,10 @@ static const char scancode_table[128] =
     'z','x','c','v','b','n','m',',','.','/',0,'*',0,' ',
 };
 
-void irq_handler(struct registers *r)
-{
-    unsigned char irq = r->int_no - 32;
-
-    if (irq == 1)
-    {
-        terminal_write("Keyboard IRQ\n");
+void irq_handler(registers_t *r) {
+    if (r->int_no == 33) {
+        terminal_write("IRQ 1 (keyboard)\n");
     }
 
-    pic_send_eoi(irq);
+    pic_send_eoi(r->int_no - 32);
 }
