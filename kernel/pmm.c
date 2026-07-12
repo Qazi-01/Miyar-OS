1599117ca25fdabcf373c5309fde5e250e99d349
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-12 22:52:57 +0000

    reserved frames setup

--- kernel/pmm.c
+++ kernel/pmm.c
@@ -12,6 +12,7 @@ static void bitmap_clear(void);
 static void bitmap_set(uint32_t frame);
 static void bitmap_reset(uint32_t frame);
 static int bitmap_test(uint32_t frame);
+static void reserve_region(uint64_t base, uint64_t length)
 
 void pmm_init(void)
 {
@@ -31,6 +32,14 @@ void pmm_init(void)
     }
 
     total_frames = usable_memory/PAGE_SIZE;
+
+    for (uint32_t i = 0; i < memory_region_count; i++)
+    {
+        if (memory_regions[i].type != 1)
+        {
+            reserve_region(memory_regions[i].base, memory_regions[i].length);
+        }
+    }
 }
 
 uint64_t pmm_total_memory(void)
@@ -71,4 +80,15 @@ static void bitmap_reset(uint32_t frame)
 static int bitmap_test(uint32_t frame)
 {
     return frame_bitmap[frame/8] & (1 << (frame % 8));
+}
+
+static void reserve_region(uint64_t base, uint64_t length)
+{
+    uint32_t first_frame = base / PAGE_SIZE;
+    uint32_t last_frame = (base + length + PAGE_SIZE - 1)/PAGE_SIZE;
+
+    for (uint32_t frame = first_frame; frame < last_frame; frame++)
+    {
+        bitmap_set(frame);
+    }
 }
