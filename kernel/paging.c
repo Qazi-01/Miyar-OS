e1c4c62ccc797b0815081126b900f6f8400a1750
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-15 22:14:37 +0500

    Refactor paging_init function for clarity
    Refactor paging initialization and improve readability.

--- kernel/paging.c
+++ kernel/paging.c
@@ -16,8 +16,11 @@ static void load_page_directory(uint32_t *page_directory)
 static void enable_paging(void)
 {
     uint32_t cr0;
+
     __asm__ volatile("mov %%cr0, %0" : "=r"(cr0));
+
     cr0 |= 0x80000000;
+
     __asm__ volatile("mov %0, %%cr0" :: "r"(cr0));
 }
 
@@ -28,20 +31,18 @@ void paging_init(void)
         first_page_table[i] = (i * 0x1000) | PAGE_PRESENT | PAGE_WRITABLE;
     }
 
-    for (uint32_t i = 0; i < 1024; i++)
-    {
-    first_page_table[i] = (i * 0x1000) | PAGE_PRESENT | PAGE_WRITABLE;
-    }
-
     uint32_t start_page = (uintptr_t)&_kernel_start / 0x1000;
     uint32_t end_page = ((uintptr_t)&_kernel_end + 0xFFF) / 0x1000;
+
     for (uint32_t i = start_page; i < end_page; i++)
     {
-    first_page_table[i] &= ~PAGE_WRITABLE;
+        first_page_table[i] &= ~PAGE_WRITABLE;
     }
-    page_directory[0] = ((uint32_t)first_page_table) | PAGE_PRESENT | PAGE_WRITABLE;
 
-    page_directory[0] = ((uint32_t)first_page_table) | PAGE_PRESENT | PAGE_WRITABLE;
+    page_directory[0] =
+        ((uint32_t)first_page_table) |
+        PAGE_PRESENT |
+        PAGE_WRITABLE;
 
     for (uint32_t i = 1; i < 1024; i++)
     {
