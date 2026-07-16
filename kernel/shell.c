52fe069840355cf39a27f84aadc168c960ae52df
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-16 12:52:38 +0000

    shell clean refactor

--- kernel/shell.c
+++ kernel/shell.c
@@ -105,6 +105,112 @@ static void split_command(const char *input, char *command, char *args)
     *args = '\0';
 }
 
+typedef void (*command_func_t)(const char *args);
+
+struct shell_command
+{
+    const char *name;
+    command_func_t handler;
+};
+
+static void cmd_help(const char *args)
+{
+    (void)args;
+
+    terminal_writeIn("");
+    terminal_writeIn("Available commands: ");
+    terminal_writeIn("-------------------\n");
+    terminal_writeIn("  help            Shows this help message");
+    terminal_writeIn("  about           About MiyarOS");
+    terminal_writeIn("  echo <msg>      Print text");
+    terminal_writeIn("  uptime          Shows system uptime");
+    terminal_writeIn("  clear           Clear the screen");
+    terminal_writeIn("  reboot          Restart the system");
+    terminal_writeIn("  shutdown        Halt the system");
+    terminal_writeIn("");
+}
+
+static void cmd_about(const char *args)
+{
+    (void)args;
+
+    terminal_writeIn("");
+    terminal_writeIn("MiyarOS v0.2");
+    terminal_writeIn("Kernel: 0.2");
+    terminal_writeIn("Architecture: x86(32-bits)");
+    terminal_writeIn("");
+    terminal_writeIn("A hobby operating system written from scratch");
+    terminal_writeIn("in C and x86 Assembly.");
+    terminal_writeIn("");
+    terminal_writeIn("Author: Tashfeen Miyar");
+    terminal_writeIn("License: GNU General Public License V3.0(GPLv3)");
+    terminal_writeIn("");
+}
+
+static void cmd_echo(const char *args)
+{
+    terminal_writeIn(args);
+}
+
+static void cmd_clear(const char *args)
+{
+    (void)args;
+    terminal_clear();
+}
+
+static void cmd_uptime(const char *args)
+{
+    (void)args;
+
+    uint32_t seconds = timer_ticks()/100;
+
+    write_uint32(seconds);
+    terminal_writeIn(" seconds");
+}
+
+static void cmd_reboot(const char *args)
+{
+    (void)args;
+    reboot_system();
+}
+
+static void cmd_shutdown(const char *args)
+{
+    (void)args;
+    shutdown_system();
+}
+
+static void cmd_exception(const char *args)
+{
+    (void)args;
+
+    terminal_writeIn("------------------------------------------------------------");
+    terminal_writeIn("");
+    terminal_writeIn("                     KERNEL PANIC");
+    terminal_writeIn("");
+    terminal_writeIn("------------------------------------------------------------");
+    terminal_writeIn("");
+    terminal_writeIn("");
+    terminal_writeIn("The system has been halted.");
+    terminal_writeIn("Restart the machine to continue.");
+    terminal_writeIn("");
+
+    trigger_divide_error();
+}
+
+static const struct shell_command command_table[] = {
+    { "help", cmd_help },
+    { "about", cmd_about },
+    { "echo", cmd_echo },
+    { "clear", cmd_clear },
+    { "exception", cmd_exception },
+    { "reboot", cmd_reboot },
+    { "shutdown", cmd_shutdown },
+    { "uptime", cmd_uptime }
+};
+
+#define COMMAND_COUNT (sizeof(command_table) / sizeof(command_table[0]))
+
 void shell_execute(const char *input)
 {
     if (*input == '\0')
@@ -119,95 +225,18 @@ void shell_execute(const char *input)
 
     terminal_writeIn("");
 
-    if (streq(command, "help"))
+    for (uint32_t i = 0; i < COMMAND_COUNT; i++)
     {
-        terminal_writeIn("");
-        terminal_writeIn("Available commands: ");
-        terminal_writeIn("-------------------\n");
-        terminal_writeIn("  help         Shows this help message");
-        terminal_writeIn("  about        About MiyarOS");
-        terminal_writeIn("  version      Show system version");
-        terminal_writeIn("  echo <msg>   Print text");
-        terminal_writeIn("  uptime       Show system uptime");
-        terminal_writeIn("  clear        Clear the screen");
-        terminal_writeIn("  reboot       Restart the system");
-        terminal_writeIn("  shutdown     Halt the system");
-        terminal_writeIn("");
+        if (streq(command, command_table[i].name))
+        {
+            command_table[i].handler(args);
+            return;
+        }
     }
-
-    else if (streq(command, "about"))
-    {
-        terminal_writeIn("");
-        terminal_writeIn("MiyarOS v0.2");
-        terminal_writeIn("");
-        terminal_writeIn("A hobby operating system written from scratch");
-        terminal_writeIn("in C and x86 Assembly.");
-        terminal_writeIn("");
-        terminal_writeIn("Author: Tashfeen Miyar");
-        terminal_writeIn("License: GNU General Public License V3.0 (GPLv3)");
-        terminal_writeIn("");
-    }
-
-    else if (streq(command, "version"))
-    {
-        terminal_writeIn("");
-        terminal_writeIn("MiyarOS version 0.2");
-        terminal_writeIn("");
-        terminal_writeIn("Kernel: 0.2");
-        terminal_writeIn("Architecture: x86 (32-bit)");
-        terminal_writeIn("");
-    }
-
-    else if (streq(command, "echo"))
-    {
-        terminal_writeIn(args);
-    }
-
-    else if (streq(command, "clear"))
-    {
-        terminal_clear();
-    }
-
-    else if (streq(command, "exception"))
-    {
-        terminal_writeIn("------------------------------------------------------------");
-        terminal_writeIn("");
-        terminal_writeIn("                    KERNEL PANIC");
-        terminal_writeIn("");
-        terminal_writeIn("------------------------------------------------------------");
-        terminal_writeIn("");
-        terminal_writeIn("");
-        terminal_writeIn("The system has been halted.");
-        terminal_writeIn("Restart the machine to continue.");
-        terminal_writeIn("");
-
-        trigger_divide_error();
-    }
-
-    else if (streq(command, "reboot"))
-    {
-        reboot_system();
-    }
-
-    else if (streq(command, "shutdown"))
-    {
-        shutdown_system();
-    }
-
-    else if (streq(command, "uptime"))
-    {
-        uint32_t seconds = timer_ticks() / 100;
-
-        write_uint32(seconds);
-        terminal_writeIn(" seconds");
-    }
-
-    else
-    {
-        terminal_write("Unknown command: ");
-        terminal_writeIn(input);
-        terminal_writeIn("");
-        terminal_writeIn("Type \"help\" for a list of available commands.");
-        terminal_writeIn("");
-    }
+    
+    terminal_write("Unknown command: ");
+    terminal_writeIn(input);
+    terminal_writeIn("");
+    terminal_writeIn("Type \"help\" for a list of available commands.");
+    terminal_writeIn("");
 }
