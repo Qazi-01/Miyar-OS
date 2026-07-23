49e9822ddff3387191aeba46117aabf79a7deed9
Tashfeen Miyar <qazitashfeen001@gmail.com>
2026-07-23 18:07:36 +0000

    Complete kernel string library

--- kernel/common/string.c
+++ kernel/common/string.c
@@ -65,3 +65,50 @@ int strcmp(const char *a, const char *b)
 
     return (unsigned char)*a - (unsigned char)*b;
 }
+
+char *strcpy(char *dest, const char *src)
+{
+    char *start = dest;
+
+    while ((*dest++ = *src++) != '\0')
+    {
+    }
+
+    return start;
+}
+
+char *strncpy(char *dest, const char *src, size_t count)
+{
+    char *start = dest;
+
+    while (count && *src)
+    {
+        *dest++ = *src++;
+        count--;
+    }
+
+    while (count)
+    {
+        *dest++ = '\0';
+        count--;
+    }
+
+    return start;
+}
+
+int strncmp(const char *a, const char *b, size_t count)
+{
+    while (count && *a && (*a == *b))
+    {
+        a++;
+        b++;
+        count--;
+    }
+
+    if (count == 0)
+    {
+        return 0;
+    }
+
+    return (unsigned char)*a - (unsigned char)*b;
+}
